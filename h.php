b27f38ca9680cb6cf6101cd0d398acd41db1f442
PARTHIV PATEL <parthivpatel14398@gmail.com>
2018-03-11 20:22:32 +0530

    Update h.php

--- h.php
+++ h.php
@@ -1,6 +1,43 @@
 
  <!-- <link rel="stylesheet" type="text/css" href="styles/home.css"> -->
+<style type="text/css">
+	  .dropdown-submenu {
+    position: relative;
+}
 
+.dropdown-submenu>.dropdown-menu {
+    top: 0;
+    left: 100%;
+    margin-top: -6px;
+    margin-left: -1px;
+    -webkit-border-radius: 0 6px 6px 6px;
+    -moz-border-radius: 0 6px 6px;
+    border-radius: 0 6px 6px 6px;
+}
+
+.dropdown-submenu:hover>.dropdown-menu {
+    display: block;
+}
+
+.dropdown-submenu>a:after {
+    display: block;
+    content: " ";
+    float: right;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    border-width: 5px 0 5px 5px;
+    border-left-color: #ccc;
+    margin-top: 5px;
+    margin-right: -10px;
+}
+
+.dropdown-submenu:hover>a:after {
+    border-left-color: #000;
+}
+</style>
+	
 		<nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -16,28 +53,7 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <!-- <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group" >
-          <input type="text" class="form-control" placeholder="Search" style="margin-top: 0;">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form> -->
+    
       <ul class="nav navbar-nav navbar-right">
         <li><a href="index.php">Home</a></li>
         
@@ -54,7 +70,7 @@
             <li><a href="#">Visitor Management</a></li>
             <li><a href="#">Photo Gallery</a></li>
             <li><a href="#">Document Management</a></li>
-           <!--  <li><a href="#">Member Detail</a></li>
+ 
             <li role="separator" class="divider"></li>
             <li><a href="#">E-services</a></li>
             <li class="dropdown dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">House Status<span class="caret"></span></a>
@@ -74,7 +90,7 @@
                 </ul>
               </li>
             <li role="separator" class="divider"></li>
-<!--            <li><a href="#">E-services</a></li>-->
+
               <li class="dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">E-service</a>
               <ul class="dropdown-menu">
                                     
@@ -91,19 +107,7 @@
                   <li><a tabindex='-1' href="#">Renter</a></li>
                                                                       
                 </ul>
-<!--
-                <ul class='dropdown-menu'>
-                    <li class = 'dropdown-submenu'><a tabindex='-1' href='#'><span class = 'glyphicon glyphicon-cog'> </span>  Transaction</a>
-                        <ul class='dropdown-menu' role = 'menu'>
-                            <li><a tabindex='-1' href='#'> Withdrawal / Deposit </a></li>
-                            <li><a tabindex='-1' href='#'> Fixed Deposit </a></li>
-                        </ul>
-                    </li>
-                    <li role='separator' class='divider'></li>
-                    <li><a href='#'><span class='glyphicon glyphicon-list-alt'> </span> Summaries </a></li>
-                    <li><a href='#'><span class='glyphicon glyphicon-wrench'> </span>  Settings </a></li>
-                </ul>
--->
+
               </li>
             <li><a href="#">Payment</a></li>
              
